6941edd67b9ed5cfd84b47c8420c82dbba78d716
Alonso Mendez <alonsomendez@gmail.com>
2019-03-03 16:07:36 -0600

    fix: Code refactoring, simplified functions

--- src/app.js
+++ src/app.js
@@ -6,50 +6,41 @@ let args = process.argv.slice(2),
     server = require('live-server'),
     lang = 'es',
     root = './src/',
-    dist = './dist/';
+    dist = './dist/',
+    icons = {
+        url: '🔗', book: '📕', course: '🎓', video: '▶️'
+    };
 
-let icons = { url: '🔗', book: '📕', course: '🎓', video: '▶️' };
-let data =  loadYAML(root + lang + '/index.yml');
 let locals = {
         version: '0.0.1',
-        lang: 'en',
+        lang: lang,
         root: root + lang + '/',
         pretty: ' ',
-        data: data,
+        data: loadYAML(root + lang + '/index.yml'),
         icons: icons,
         loadYAML: loadYAML
     };
+
 let params = {
         root: dist,
         port: 8888,
         wait: 500,
-        logLevel: 1
+        logLevel: 2
     };
 
+fs.existsSync(dist) || fs.mkdir(dist, { recursive: true } );
 
-if (!fs.existsSync(dist)){
-    fs.mkdir(dist, { recursive: true });
-}
-
-if ( args.indexOf('build') >= 0 ) {
-    compile();
-}
+compile();
 
 if ( args.indexOf('watch') >= 0 ) {
-    compile();
-
     server.start(params);
     watch(root, { recursive: true }, compile);
 }
 
 function compile() {
-    try {
-        let compiler = pug.compileFile(root + 'index.pug', {});
+    let compiler = pug.compileFile(root + 'index.pug');
 
-        fs.writeFile(dist + 'index.html', compiler(locals));
-    } catch (e) {
-        console.log('Error:', e);
-    }
+    fs.writeFile(dist + 'index.html', compiler(locals));
 }
 
 function loadYAML(file) {
