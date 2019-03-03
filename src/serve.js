b0b04f42a390cec73355313c1ab344b5b68e5c71
Alonso Mendez <alonsomendez@gmail.com>
2019-03-03 04:33:14 -0600

    feat: Added yaml compiler and basic trunk in spanish

--- src/serve.js
+++ src/serve.js
@@ -1,3 +1,57 @@
-let yaml = require('js-yaml');
+let fs   = require('fs'),
+    pug = require('pug'),
+    yaml = require('js-yaml'),
+    watch = require('node-watch'),
+    StaticServer = require('static-server'),
+    lang = 'es',
+    icons = { url: '🔗', book: '📕', course: '🎓', video: '▶️' },
+    root = './src/',
+    dist = './dist';
 
-console.log("Test");
+if (!fs.existsSync(dist)){
+    fs.mkdir(dist, { recursive: true });
+}
+
+watch(root, { recursive: true }, compile);
+
+let server = new StaticServer({
+    rootPath: dist,
+    port: 8888
+});
+
+server.start(function () {
+    console.log('Server listening to', server.port);
+});
+
+function compile() {
+    try {
+        let map = yaml.safeLoad(fs.readFileSync(root + lang + '/index.yml', 'utf8'));
+        let options = {};
+        let locals = {
+            version: '0.0.1',
+            lang: 'en',
+            root: root + lang + '/',
+            pretty: ' ',
+            map: map,
+            icons: icons,
+            loadYAML: loadYAML
+        };
+
+        let compiler = pug.compileFile('src/index.pug', options);
+        let html = compiler(locals);
+
+        fs.writeFile('dist/index.html', html, function(err) {
+            if(err) {
+                throw err;
+            } else {
+                console.log('The file was saved!');
+            }
+        });
+    } catch (e) {
+        console.log('Error:', e);
+    }
+}
+
+function loadYAML(file) {
+    return yaml.safeLoad(fs.readFileSync(file, 'utf8'));
+}
